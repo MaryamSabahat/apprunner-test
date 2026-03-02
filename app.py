c32d080628613d1eeb1fc0814aebd0fe25a6bc5d
Maryam Sabahat <96340288+MaryamSabahat@users.noreply.github.com>
2026-03-03 02:58:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World from AppRunner!"}
+    # DIAGNOSTIC: Returns the files found in the current directory
+    return {"files_found": os.listdir(".")}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
